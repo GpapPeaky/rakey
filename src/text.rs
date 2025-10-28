5300ab6c59724ea76451953adcea88dfa7bcd1db
GpapPeaky <giorgospapamat@gmail.com>
2025-10-28 17:31:04 +0200

    Setup for c keyword highlighting.

---
+++ src/text.rs
@@ -0,0 +1,49 @@
+const c_control_flow_statements: [&str ; 12] = [
+    "if",
+    "else",
+    "switch",
+    "case",
+    "default",
+    "for",
+    "while",
+    "do",
+    "break",
+    "continue",
+    "goto",
+    "return"
+];
+
+const c_storage_class_specifiers: [&str ; 5] = [
+    "auto",
+    "static",
+    "extern",
+    "register",
+    "typedef"
+];
+
+const c_type_qualifiers: [&str ; 1] = [
+    "const"
+];
+
+const c_composite_types: [&str ; 3] = [
+    "struct",
+    "union",
+    "enum"
+];
+
+const c_misc: [&str ; 2] = [
+    "sizeof",
+    "inline"
+];
+
+const c_data_types: [&str ; 9] = [
+    "int",
+    "float",
+    "double",
+    "char",
+    "void",
+    "short",
+    "long",
+    "signed",
+    "unsigned"
+];
